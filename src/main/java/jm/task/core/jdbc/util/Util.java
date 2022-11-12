e757c02bb592587149ea5b5a2cd41bf9116f449a
Vladislav Smirnov <vatka21@mail.ru>
2022-11-12 09:42:37 +0300

    Add Data Base connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/users_table";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "Ghbynth_vjybnjh_99";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            connection = con;
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
